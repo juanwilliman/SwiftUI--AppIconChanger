bff418f72042df72493df56de468125db57be8cb
Juan Williman <juanwilliman@yahoo.com>
2024-05-24 23:55:01 -0300

    Updates

--- AppIconChanger/AppIconChanger/ContentView.swift
+++ AppIconChanger/AppIconChanger/ContentView.swift
@@ -18,20 +18,28 @@ struct ContentView: View {
     // MARK: - Body
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 50) {
-            ForEach(AppIcon.allCases, id: \.self) { appIcon in
-                Button(action: { themeController.selectAppIcon(appIcon) }) {
-                    VStack {
-                        Image(appIcon.rawValue)
-                            .resizable()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .cornerRadius(10)
-                        Text(appIcon.rawValue)
+        NavigationStack {
+            HStack(spacing: 50) {
+                ForEach(AppIcon.allCases, id: \.self) { appIcon in
+                    Button(action: { themeController.selectAppIcon(appIcon) }) {
+                        VStack(spacing: 10) {
+                            Image(appIcon.rawValue)
+                                .resizable()
+                                .frame(width: 70, height: 70, alignment: .center)
+                                .cornerRadius(18)
+                                .shadow(color: .black.opacity(0.15), radius: 10, x: 0, y: 10)
+                            Text(appIcon.rawValue)
+                                .foregroundStyle(.primary)
+                                .padding(.top, 10)
+                            Image(systemName: themeController.selectedAppIcon == appIcon ? "checkmark.circle.fill" : "circle")
+                                .foregroundStyle(Color.accentColor)
+                        }
                     }
+                    .buttonStyle(PlainButtonStyle())
                 }
             }
+            .navigationTitle("Icon Changer")
         }
-        .padding(25)
     }
 }
 
